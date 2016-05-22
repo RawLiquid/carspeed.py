0bc60a461def0caec195a1b1ef4405323424bf38
RawLiquid <rawliquid@gmail.com>
2016-05-21 22:12:28 -0700

    Correcting video orientation for my setup.

--- carspeed.py
+++ carspeed.py
@@ -108,8 +108,8 @@ prompt = ''
 camera = PiCamera()
 camera.resolution = RESOLUTION
 camera.framerate = FPS
-camera.vflip = True
-camera.hflip = True
+camera.vflip = False
+camera.hflip = False
 
 rawCapture = PiRGBArray(camera, size=camera.resolution)
 # allow the camera to warm up
